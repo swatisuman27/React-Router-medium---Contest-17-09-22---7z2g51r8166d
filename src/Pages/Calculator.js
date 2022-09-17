2e1767256367e4927433f77e646cd6f6c5a9a495
Swati Suman <104189142+swatisuman27@users.noreply.github.com>
2022-09-17 18:15:17 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -9,15 +9,15 @@ export const Calculator = () =>{
     var num1 = Number(url.searchParams.get("num1"));
     var num2 = Number(url.searchParams.get("num2"));
     var op = url.searchParams.get("op");
-
+    console.log(num1, num2, op);
     let answer = 0;
-
+    console.log(op);
     switch(op){
-        case"+": {answer = num1 + num2; break;}
-        case"-": {answer = num1 - num2; break;}
-        case"*": {answer = num1 * num2; break;}
-        case"/": {answer = num1 / num2; break;}
-        case"%": {answer = num1 % num2; break;}
+        case "+": {answer = num1 + num2; break;}
+        case "-": {answer = num1 - num2; break;}
+        case "*": {answer = num1 * num2; break;}
+        case "/": {answer = num1 / num2; break;}
+        case "%": {answer = num1 % num2; break;}
     }
 
     return(
